a4196b13fc704392d519c45187637b3aec4b4379
Manalbariz <manal.bariz@epitech.eu>
2024-01-08 17:42:58 +0100

    Advc bases

--- dist/index.js
+++ dist/index.js
@@ -20,3 +20,87 @@ user.push(5);
 console.log(user);
 console.log(typeof user[0]);
 ////////////
+//////////// enums
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 0] = "Small";
+    Size[Size["Medium"] = 1] = "Medium";
+    Size[Size["Large"] = 2] = "Large";
+})(Size || (Size = {}));
+;
+var Size2;
+(function (Size2) {
+    Size2["Small"] = "S";
+    Size2["Medium"] = "M";
+    Size2["Large"] = "L";
+})(Size2 || (Size2 = {}));
+;
+let firstClient = Size.Small;
+let secondClient = Size2.Medium;
+console.log(Size);
+console.log(firstClient); //return the index
+console.log(secondClient); //return the index 
+var days;
+(function (days) {
+    days[days["day1"] = 1] = "day1";
+    days[days["day2"] = 2] = "day2";
+    days[days["day3"] = 3] = "day3";
+})(days || (days = {}));
+; //The second value is automatically 2     // days[days["day1"] = 1] = "day1"   // days[days["day2"] = 2] = "day2"
+console.log(days.day2);
+console.log(days);
+////////////
+//////////// functions
+function add(firstNumber, secondNumber) {
+    if (firstNumber > secondNumber)
+        return firstNumber + secondNumber;
+    else
+        return 0;
+}
+function add2(firstNumber, secondNumber, thirdNumber) {
+    if (!thirdNumber)
+        return firstNumber + secondNumber;
+    else if (thirdNumber)
+        return firstNumber + secondNumber + thirdNumber;
+    else
+        return 0;
+}
+console.log(add(3, 5));
+console.log(add(5, 3));
+console.log(add2(7, 3));
+console.log(add2(5, 3, 5));
+////////////
+//////////// Objects
+let client = { num: 1, age: 10, name: 'Sam' };
+client.name = 'Sara';
+// client.age = 50;   we add readonly if we don't wanna change the value
+console.log(client);
+console.log(client.name);
+let employers = [{ name: 'alecia', id: 0, age: 30 },];
+console.log(employers);
+console.log(employers[0]);
+employers.push({ name: 'alex', id: 1, age: 26 });
+console.log(employers);
+console.log(employers[1]);
+let client1 = { num: 1, age: 10, name: 'Sam' };
+let client2 = { num: 2, age: 20, name: 'Sara' };
+////////////
+//////////// Union types
+function isItaNumberOraString(parameter) {
+    if (typeof parameter === 'number')
+        console.log('It is a number');
+    else if (typeof parameter === 'string')
+        console.log('It is a string');
+    else
+        console.log('Are u kidding me');
+}
+isItaNumberOraString(5);
+isItaNumberOraString('hey');
+isItaNumberOraString(true);
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let age1;
+age1 = 30;
+////////////
--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,136 @@ console.log(typeof user[0]);
 
 
 ////////////
+
+//////////// enums
+
+enum Size {Small, Medium, Large};
+enum Size2 {Small= "S", Medium= "M", Large= "L"};
+
+
+let firstClient: Size = Size.Small;
+let secondClient: Size2 = Size2.Medium;
+
+console.log(Size);
+
+
+
+console.log(firstClient);  //return the index
+console.log(secondClient);  //return the index 
+
+enum days { day1=1 , day2, day3};    //The second value is automatically 2     // days[days["day1"] = 1] = "day1"   // days[days["day2"] = 2] = "day2"
+
+console.log(days.day2);
+console.log(days);
+
+
+
+
+////////////
+
+
+//////////// functions
+
+function add(firstNumber: number, secondNumber: number){
+    if (firstNumber > secondNumber)
+        return firstNumber + secondNumber ;
+    else 
+        return 0;
+}
+
+function add2(firstNumber: number, secondNumber: number, thirdNumber? : number){            //? : to make a parameter optional
+    if (!thirdNumber)
+        return firstNumber + secondNumber ;
+    else if(thirdNumber)
+        return firstNumber + secondNumber + thirdNumber;
+    else
+        return 0;
+}
+
+console.log(add(3,5));
+console.log(add(5,3));
+console.log(add2(7,3));
+console.log(add2(5,3,5));
+
+
+////////////
+
+//////////// Objects
+let client : {num : number, readonly age : number, name : string} = { num : 1, age : 10, name : 'Sam'};
+
+client.name = 'Sara';
+// client.age = 50;   we add readonly if we don't wanna change the value
+console.log(client);
+console.log(client.name);
+
+let employers : object[] = [{name : 'alecia', id : 0, age : 30 }, ];
+
+
+
+console.log(employers);
+console.log(employers[0]);
+
+employers.push({name : 'alex', id : 1, age : 26 }) ;
+console.log(employers);
+console.log(employers[1]);
+
+
+
+////////////
+
+
+//////////// type aliases
+
+type Client = {num : number, readonly age : number, name : string};
+type numberOrString = string | number;
+
+
+let client1 : Client= { num : 1, age : 10, name : 'Sam'};
+let client2 : Client = { num : 2, age : 20, name : 'Sara'};
+
+
+
+////////////
+
+//////////// Union types
+
+function isItaNumberOraString( parameter : Number | String | any ) {   // the type of the parameter can be one of the types declared
+    if (typeof parameter === 'number')
+        console.log('It is a number');
+    else if (typeof parameter === 'string')
+        console.log('It is a string');
+    else 
+        console.log('Are u kidding me');
+        
+
+    
+}
+
+isItaNumberOraString(5);
+isItaNumberOraString('hey');
+isItaNumberOraString(true);
+
+
+////////////
+
+//////////// Intersection types
+
+type Draggable = { drag: () => void};
+type Resizable = { resize: () => void};
+
+type UIWidget = Draggable & Resizable ;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {},
+}
+////////////
+
+//////////// Literal types
+
+type Age = 30 | 31;
+
+let age1: Age ;
+
+age1 = 30;
+////////////
